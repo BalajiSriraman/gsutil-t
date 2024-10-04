629a4c0f45dc71a5240a599a6da16abafcf71bc6
BalajiSriraman <balajisrdev@gmail.com>
2024-10-04 14:50:36 +0530

    Refactor script to download files from URLs

--- exec.bat
+++ exec.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 set "urlFile=urls.txt"
 if not exist "%urlFile%" (
@@ -7,11 +7,28 @@ if not exist "%urlFile%" (
     exit /b 1
 )
 
+set count=0
+set batch=
+
 for /f "usebackq delims=" %%i in ("%urlFile%") do (
-    echo Downloading: %%i
-    gsutil -m cp -r "%%i" f:\mail2024.
+    set /a count+=1
+    set "batch=!batch! %%i"
+    if !count! equ 10 (
+        echo Downloading batch...
+        gsutil -m cp -r !batch! f:\mail2024.
+        if errorlevel 1 (
+            echo Error occurred while downloading batch
+        )
+        set count=0
+        set batch=
+    )
+)
+
+if !count! neq 0 (
+    echo Downloading final batch...
+    gsutil -m cp -r !batch! f:\mail2024.
     if errorlevel 1 (
-        echo Error occurred while downloading: %%i
+        echo Error occurred while downloading final batch
     )
 )
 
